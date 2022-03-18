a16a1bb15b93f8def4af685cc62a129433a33499
Ran-n <ran-n@tutanota.com>
2022-03-18 17:52:14 +0100

    cambios

--- src/coinmarketcap_scrapi/__init__.py
+++ src/coinmarketcap_scrapi/__init__.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2022/03/18 16:19:46.685688
-#+ Editado:	2022/03/18 16:55:01.387918
+#+ Editado:	2022/03/18 17:51:52.671580
 # ------------------------------------------------------------------------------
 
 '''
@@ -13,7 +13,6 @@ info coinmarketcap_scrapi
 # ------------------------------------------------------------------------------
 
 from .coinmarketcap_scrapi import CoinMarketCap
-#from .__variables import RAIZ
 
 # ------------------------------------------------------------------------------
 
@@ -22,7 +21,6 @@ __version__ = '1.0'
 __autor__ = 'Ran#'
 __creditos__ = 'Ran#'
 __licenza__ = 'GPLv3'
-#__raiz__ = RAIZ
 __descricion__ = 'Using the scraped keyless endpoints of the CoinMarketCap website'
 __ligazon__ = 'https://wwww.github.com/Ran-n/coinmarketcap_scrapi'
 __problemas__ = 'https://wwww.github.com/Ran-n/coinmarketcap_scrapi/issues'
